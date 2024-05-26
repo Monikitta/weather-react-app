388f32f0b6a50165189203375db0e557cd674b7e
Monika <monika.sarafova9111@gmail.com>
2024-05-26 22:33:36 +0300

    adding useCallback and useMemo react hooks in Context.js file

--- src/Context.js
+++ src/Context.js
@@ -1,3 +1,76 @@
+import React, { createContext, useState, useEffect, useCallback, useMemo } from 'react';
+import cloudy from "./assets/Cloudy.svg";
+
+export const WeatherContext = createContext();
+
+export const WeatherProvider = ({ children }) => {
+  const [weather, setWeather] = useState({
+    city: 'London',
+    temp: '16',
+    temp_max: '19',
+    temp_min: '15',
+    humidity: '58',
+    description: 'THUNDERSTORM WITH LIGHT DRIZZLE',
+    cloudPercentage: '86',
+    wind: '5',
+    icon: cloudy,
+    forecast: [
+      {
+        dt: Date.now(),
+        main: {
+          temp: '19',
+        },
+        weather: [{
+          description: 'Snow',
+        }]
+      },
+    ]
+  });
+
+  const [showHistory, setShowHistory] = useState(false);
+
+  const initialHistory = useMemo(() => {
+    const savedHistory = localStorage.getItem('searchHistory');
+    return savedHistory ? JSON.parse(savedHistory) : [];
+  }, []);
+
+  const [history, setHistory] = useState(initialHistory);
+
+  useEffect(() => {
+    try {
+      localStorage.setItem('searchHistory', JSON.stringify(history));
+    } catch (error) {
+      if (error.name === 'QuotaExceededError') {
+        alert("Local storage limit exceeded!");
+      }
+      console.error("Error saving to local storage", error);
+    }
+  }, [history]);
+
+  const toggleHistory = useCallback(() => {
+    setShowHistory(prev => !prev);
+  }, []);
+
+  const addToHistory = useCallback((city) => {
+    if (!history.includes(city)) {
+      setHistory(prevHistory => [...prevHistory, city]);
+    }
+  }, [history]);
+
+  const clearHistory = useCallback(() => {
+    localStorage.removeItem('searchHistory');
+    setHistory([]);
+  }, []);
+
+  return (
+    <WeatherContext.Provider value={{ weather, setWeather, showHistory, toggleHistory, history, addToHistory, clearHistory }}>
+      {children}
+    </WeatherContext.Provider>
+  );
+};
+
+
+/*
 import React, { createContext, useState, useEffect } from 'react';
 import cloudy from "./assets/Cloudy.svg";
 
@@ -65,3 +138,5 @@ export const WeatherProvider = ({ children }) => {
     </WeatherContext.Provider>
   );
 };
+
+*/
